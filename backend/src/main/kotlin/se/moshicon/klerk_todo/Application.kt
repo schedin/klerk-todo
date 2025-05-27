09e09400feb8a56ccb7c3576e92bd4132b422857
Lennart Schedin <lennart.schedin@gmail.com>
2025-05-27 08:09:11 +0200

    Start of adding initial data

--- backend/src/main/kotlin/se/moshicon/klerk_todo/Application.kt
+++ backend/src/main/kotlin/se/moshicon/klerk_todo/Application.kt
@@ -2,6 +2,7 @@ package se.moshicon.klerk_todo
 
 import dev.klerkframework.klerk.AuthenticationIdentity
 import dev.klerkframework.klerk.Klerk
+import dev.klerkframework.klerk.SystemIdentity
 import dev.klerkframework.klerk.command.Command
 import dev.klerkframework.klerk.command.CommandToken
 import dev.klerkframework.klerk.command.ProcessingOptions
@@ -30,6 +31,7 @@ fun main() {
     runBlocking {
         klerk.meta.start()
         createInitialUsers(klerk)
+        createInitialTodo(klerk)
     }
 
 //    performanceInsertTest(klerk)
@@ -78,6 +80,33 @@ fun main() {
 
 
     }.start(wait = true)
+}
+
+suspend fun createInitialTodo(klerk: Klerk<Ctx, Data>) {
+    val context = Ctx(SystemIdentity)
+    val numberOfTodos = klerk.read(context) {
+        list(data.todos.all).size
+    }
+    if (numberOfTodos != 0) {
+        return
+    }
+
+    val user = (context.actor as? GroupModelIdentity)?.model?.props
+        ?: throw IllegalStateException("User not found in context")
+
+    val command = Command(
+        event = CreateTodo,
+        model = null,
+        params = CreateTodoParams(
+            title = TodoTitle("Buy milk"),
+            description = TodoDescription("Go to the store and buy milk"),
+            username = user.name,
+            priority = TodoPriority(6),
+        ),
+    )
+
+    klerk.handle(command, context, ProcessingOptions(CommandToken.simple()))
+
 }
 
 fun performanceInsertTest(klerk: Klerk<Ctx, Data>) {
